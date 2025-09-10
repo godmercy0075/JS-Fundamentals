d55c081d3c26fc99573e6e810011353ff78be18f
MERCY NNAMCHI <mercynnamani123@gmail.com>
2025-09-10 19:21:34 +0100

    Add concate

---
+++ 4-concat.js
@@ -0,0 +1,5 @@
+const firstArg = process.argv[2];
+const secondArg = process.argv[3];
+console.log(firstArg + " is " + secondArg);
+// node 4-concat.js undefine is undefinedd
+// undefine is undefined    
